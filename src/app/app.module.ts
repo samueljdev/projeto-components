3d0440758e32a00d9f8d9f452fd7035e87f13614
samueljdev <samuel.jdev11@gmail.com>
2025-08-03 23:33:28 -0400

    Criação de um Componente Parte 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardRoxoComponent
   ],
   imports: [
     BrowserModule
