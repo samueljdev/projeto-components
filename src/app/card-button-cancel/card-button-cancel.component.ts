0567b5146725f1c65f9a322027cde80652e5740a
samueljdev <samuel.jdev11@gmail.com>
2025-08-07 23:18:33 -0400

    Estilos Dentro do Template (Inline Styles)

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -4,11 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
   styleUrls: ['./card-button-cancel.component.scss'],
-  /* 
-    não referenciar mais de um arquivo de estilos, nem estilos de outro componente
-    styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel-2.component.scss'],
-    styleUrls: ['./card-button-cancel.component.scss', '../card/card.component.scss'],
-  */
+  styles: [
+    `.card-button-cancel {
+        font-size: 16px;
+        text-align: center;
+        margin-top: 10px;
+        border-radius: 25px;
+        background-color: #ff4d4d;
+        cursor: pointer;
+        font-weight: bold;
+        padding: 10px;
+      }
+    `
+  ],
 })
 export class CardButtonCancelComponent {
 
