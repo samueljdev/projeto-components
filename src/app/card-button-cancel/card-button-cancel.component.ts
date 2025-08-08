6bcc0a6bd27ce29e3e2e7f76a7f42ef1f4d98ebd
samueljdev <samuel.jdev11@gmail.com>
2025-08-08 15:46:49 -0400

    Importar Arquivos de Estilos com "@import"

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -4,19 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
   styleUrls: ['./card-button-cancel.component.scss'],
-  styles: [
-    `.card-button-cancel {
-        font-size: 16px;
-        text-align: center;
-        margin-top: 10px;
-        border-radius: 25px;
-        background-color: #ff4d4d;
-        cursor: pointer;
-        font-weight: bold;
-        padding: 10px;
-      }
-    `
-  ],
 })
 export class CardButtonCancelComponent {
 
